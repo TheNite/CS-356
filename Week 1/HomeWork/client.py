560c87ba4aae07534eca4576323d088b0150d51b
Paul <paul@Pauls-MBP.noxian.net>
2020-09-02 17:29:16 -0400

    Homework 1

--- Week 1/HomeWork/client.py
+++ Week 1/HomeWork/client.py
@@ -8,17 +8,31 @@ host = sys.argv[1]
 port = int(sys.argv[2])
 count = int(sys.argv[3])
 data = 'X' * count # Initialize data to be sent
+max_tries = 3
 
 # Create UDP client socket. Note the use of SOCK_DGRAM
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientsocket.settimeout(3)
 
-# Send data to server
-print("Sending data to   " + host + ", " + str(port) + ": " + data)
-clientsocket.sendto(data.encode(),(host, port))
+while True:
 
-# Receive the server response
-dataEcho, address = clientsocket.recvfrom(count)
-print("Receive data from " + address[0] + ", " + str(address[1]) + ": " + dataEcho.decode())
+    if max_tries == 0:
+        break
+
+    # Send data to server
+    print("Sending data to   " + host + ", " + str(port) + ": " + data)
+    clientsocket.sendto(data.encode(), (host, port))
+
+    try:
+        # Receive the server response
+        dataEcho, address = clientsocket.recvfrom(count)
+        print(dataEcho, address)
+        print("Receive data from " + address[0] + ", " + str(address[1]) + ": " + dataEcho.decode())
+        break
+    except socket.timeout:
+        print(f'Message timed out')
+        max_tries -= 1
+        continue
 
 #Close the client socket
 clientsocket.close()
