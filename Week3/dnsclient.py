5916c7de8229fca64ebf9f9fec7fe548ed51c224
Paul <paul@Pauls-MacBook-Pro.local>
2020-09-22 17:50:15 -0400

    Inital variables & rename file names

---
+++ Week3/dnsclient.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+
+'''
+Paul Cruz
+psc28
+CS 356-001
+'''
+
+import sys
+import socket
+import struct
+import time
+import random
+
+# Get the server hostname, port and hostname as command line arguments
+host = sys.argv[1]
+port = int(sys.argv[2])
+hostname = sys.argv[3]
+
+'''
+2 Bytes Variables
+'''
+# Default Values
+message_type = 1
+return_code = 0
+answer_length = 0
+message_length = sys.getsizeof(hostname)
+
+'''
+4 Bytes Variables
+'''
+message_id = random.randint(1, 100)     # returns a number between 1 and 100 (both included)
+
+
+pings, count, max_sec, min_sec, avg, timeout = 10, 0, -1, 1, 0, 0
+
+
+# Create UDP client socket. Note the use of SOCK_DGRAM
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientsocket.settimeout(1)  # 1 Second timeout
+
+print(f"Pinging {host}, {port}:")
+data = struct.pack('!hhhhis', message_type, return_code, answer_length, message_length, message_id, hostname.encode())
+
+try:
+    # Send data to server
+    clientsocket.sendto(data, (host, port))
+    # Receive the server response
+    dataEcho, address = clientsocket.recvfrom(100)
+    dataEcho = struct.unpack('!hh', dataEcho)
+    t2 = time.time()
+    print(f"Ping Message number {count}, RTT: {t2 - t1:.10f} sec")
+
+except socket.timeout:
+    print(f'Ping message number {count} timed out')
+    timeout += 1
+
+# print(f'\nStatistics: \n'
+#       f'{count} packets transmitted, {count - timeout} Received, {(timeout / count) * 100}% packet loss\n'
+#       f'Min/Max/Avg RTT = {min_sec:.10f} / {max_sec:.10f} / {avg / (count - timeout):.10f} sec')
+
+# Close the client socket
+clientsocket.close()
